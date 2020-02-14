d09c1f1b058ab1b889d306e5fa18d960a4264414
Hotman2000 <acharu2000x@gmail.com>
2020-02-14 16:49:01 +0900

    発射処理にコメントを追加

--- Assets/EndouHaruhito/Script/TestShooter.cs
+++ Assets/EndouHaruhito/Script/TestShooter.cs
@@ -16,8 +16,11 @@ public class TestShooter : MonoBehaviour
     {
         if (Input.anyKey)
         {
+            //下記の40.0fを変更すれば回転角を指定できます。（ランダム値、右スティックの値など）
+            //Quaternion q = Quaternion.Euler(0.0f, 40.0f, 0.0f);
             //下の二行を実行してください。instantiateでは発射元の位置と角度を渡してください。Fireでは発射元のGameObjectと発射元が爆弾かどうかのboolを渡してください
-            Instantiate(bullet,transform.position,transform.rotation);
+            //Instantiate(bullet,transform.position,q);//角度指定するバージョン
+            Instantiate(bullet, transform.position, transform.rotation);//現在の角度で発射されるバージョン
             bullet.GetComponent<Bullet>().Fire(gameObject,true);
         }
     }
