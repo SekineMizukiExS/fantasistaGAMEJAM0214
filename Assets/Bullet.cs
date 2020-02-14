c00f8194ca180331b10744c24fc2fe13501e7cd7
SekineMizuki <mitakin29@gmail.com>
2020-02-14 15:25:31 +0900

    [Update]基盤固め中

---
+++ Assets/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    GameManager _gm;//ゲームマネージャー
+
+    [SerializeField] Vector3 _direction;//進行方向
+    [SerializeField] float _speed = 1.0f;//進行速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += _direction * _speed * _gm._nowSpeed;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameManager _gm;
+    Vector3 _inputAxis;
+    [SerializeField] float _speed = 3.0f;
+    public Quaternion rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _inputAxis.x = Input.GetAxis("Horizontal");
+        _inputAxis.y = Input.GetAxis("Vertical");
+        _inputAxis.Normalize();
+
+        transform.position += _inputAxis * _speed * _gm._nowSpeed;
+        if (_inputAxis.magnitude >= 0.1f) {
+            var velocity2D = (Vector2)_inputAxis;
+            var targetDirection = Quaternion.Euler(0.0f, 0.0f, Vector2.SignedAngle(Vector2.up, velocity2D));
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetDirection, 0.1f);
+        }
+    }
+}
